ded3e127110ab6ff4289a637aee083a348cd8f00
Bala Alagappan <61052566+albala99@users.noreply.github.com>
2022-01-05 20:15:14 +0530

    Scroll 2 fetched

--- Supersourcing/script.js
+++ Supersourcing/script.js
@@ -1,9 +1,11 @@
 const scrollcontainer=document.querySelector('.auto-scroll');
+const scrollcontainer2=document.querySelector('.auto-scroll-2');
 events();
 function events(){
     window.addEventListener('DOMContentLoaded',()=>{
         console.log("dom loaded");
         fetchjson();
+        fetchjson2();
     })
 }
 
@@ -14,6 +16,18 @@ function fetchjson(){
             html+=`<div class="figure"><figure><img src="${container.imgsrc}" alt="${container.alt}"></figure></div>`;
         })
         scrollcontainer.innerHTML=html;
+    }).catch(function(error){
+        console.log(error);
+    })
+}
+
+function fetchjson2(){
+    fetch('images1.json').then(response=>response.json()).then(data=>{
+        let html='';
+        data.forEach(container=>{
+            html+=`<div class="figure"><figure><img src="${container.imgsrc}" alt="${container.alt}"></figure></div>`;
+        })
+        scrollcontainer2.innerHTML=html;
     }).catch(function(error){
         console.log(error);
     })
