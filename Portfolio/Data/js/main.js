0da44267b4e50b20b1701d09926bc32647fc0ea9
Bala Alagappan <albala99@gmail.com>
2022-01-26 21:11:49 +0530

    Form sync to sheets

--- Portfolio/Data/js/main.js
+++ Portfolio/Data/js/main.js
@@ -132,3 +132,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+// Form
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Will reach Shortly!");
+      })
+    });
+  });
